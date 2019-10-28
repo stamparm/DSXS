1161bef6e58b6e46db776033cdde0eb5a0dd2547
Miroslav Stampar <miroslav.stampar@gmail.com>
2019-10-28 16:10:10 +0100

    Minor patch

--- dsxs.py
+++ dsxs.py
@@ -35,7 +35,7 @@ def _retrieve_content(url, data=None):
         req = urllib.request.Request("".join(url[i].replace(' ', "%20") if i > url.find('?') else url[i] for i in range(len(url))), data.encode("utf-8", "ignore") if data else None, _headers)
         retval = urllib.request.urlopen(req, timeout=TIMEOUT).read()
     except Exception as ex:
-        retval = ex.read() if hasattr(ex, "read") else getattr(ex, "msg", str())
+        retval = ex.read() if hasattr(ex, "read") else str(ex.args[-1])
     return (retval.decode("utf-8", "ignore") if hasattr(retval, "decode") else "") or ""
 
 def _contains(content, chars):
