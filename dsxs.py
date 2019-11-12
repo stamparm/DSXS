d79cc267171691488174b43cc60702e8ec6cde33
Miroslav Stampar <miroslav.stampar@gmail.com>
2019-11-12 11:56:31 +0100

    Trivial update

--- dsxs.py
+++ dsxs.py
@@ -32,11 +32,11 @@ _headers = {}                                                                   
 
 def _retrieve_content(url, data=None):
     try:
-        req = urllib.request.Request("".join(url[i].replace(' ', "%20") if i > url.find('?') else url[i] for i in range(len(url))), data.encode("utf-8", "ignore") if data else None, _headers)
+        req = urllib.request.Request("".join(url[i].replace(' ', "%20") if i > url.find('?') else url[i] for i in range(len(url))), data.encode("utf8", "ignore") if data else None, _headers)
         retval = urllib.request.urlopen(req, timeout=TIMEOUT).read()
     except Exception as ex:
         retval = ex.read() if hasattr(ex, "read") else str(ex.args[-1])
-    return (retval.decode("utf-8", "ignore") if hasattr(retval, "decode") else "") or ""
+    return (retval.decode("utf8", "ignore") if hasattr(retval, "decode") else "") or ""
 
 def _contains(content, chars):
     content = re.sub(r"\\[%s]" % re.escape("".join(chars)), "", content) if chars else content
